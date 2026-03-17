98d5529811f51fddae49e376e3c6721b016618d7
Berardelli <carlo.berardelli@sap.com>
2026-03-16 23:26:03 -0400

    removed useIsMobile from portfolio

--- techWrench/src/app/portfolio/PortfolioClient.tsx
+++ techWrench/src/app/portfolio/PortfolioClient.tsx
@@ -1,18 +1,17 @@
 "use client";
 
 import styles from "./portfolio.module.css";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import HamburgerMenu from "../components/HamburgerMenu";
-import useIsMobile from "../hooks/useIsMobile";
 import ImageViewer from "../components/ImageViewer";
 import HeroOverlay from "../components/HeroOverlay";
 
 export default function Page() {
-  const isMobile = useIsMobile(850);
   return (
     <div className={styles.wrapper}>
-      {isMobile ? <HamburgerMenu /> : <Header />}
+      <div className={styles.navBar}>
+        <Navbar />
+      </div>
       <HeroOverlay
         backgroundSrc="/artsyLogo.png"
         height="500px"
